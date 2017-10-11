69ec4d09523eae67a960b0586ea556bcd7f079f2
irfanhasan <ihasan03@gmail.com>
2017-10-11 00:26:37 -0500

    second attempt

--- push.sh
+++ push.sh
@@ -3,16 +3,20 @@
 setup_git() {
     git config --global user.email "travis@travis-ci.org"
     git config --global user.name "Travis CI"
+    mkdir temp
 }
 
 commit_files() {
+    cd temp
+    git init
+    cp -R ../.ekstazi/ .ekstazi/
     git add -f .ekstazi/*
     git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-    git remote add origin-pages https://${GH_TOKEN}@github.com/irfanhasan/travisCIcache.git 
-    git push -u origin-pages master
+    git remote add origin https://${GH_TOKEN}@github.com/irfanhasan/travisCIcache.git 
+    git push -u origin master
 }
 
 setup_git
