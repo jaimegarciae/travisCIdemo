1d466eb15c85897aecc680098609cc1b619d80a6
irfanhasan <ihasan03@gmail.com>
2017-10-22 13:40:52 -0500

    made push script pull from repo before pushing

--- push.sh
+++ push.sh
@@ -9,14 +9,16 @@ setup_git() {
 commit_files() {
     cd temp
     git init
+    git remote add origin https://${GH_TOKEN}@github.com/irfanhasan/travisCIcache.git 
+    git pull origin master
+    rm -r .ekstazi/
     cp -R ../.ekstazi/ .ekstazi/
     git add -f .ekstazi/*
     git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
 
 upload_files() {
-    git remote add origin https://${GH_TOKEN}@github.com/irfanhasan/travisCIcache.git 
-    git push origin master
+    git push -u origin master
 }
 
 setup_git
